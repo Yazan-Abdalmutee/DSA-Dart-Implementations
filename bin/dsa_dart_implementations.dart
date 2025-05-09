59973e1d5d0785db3d93f7b1267cf08d5588b0df
yazan <yazanshrouf149@gmail.com>
2025-05-09 14:08:10 +0300

    init dsa dart implementations

---
+++ bin/dsa_dart_implementations.dart
@@ -0,0 +1,5 @@
+import 'package:dsa_dart_implementations/dsa_dart_implementations.dart' as dsa_dart_implementations;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dsa_dart_implementations.calculate()}!');
+}
---
+++ test/dsa_dart_implementations_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsa_dart_implementations/dsa_dart_implementations.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
