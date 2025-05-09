aa74fb82f5ad04d131ac4570ebd1edf58f17ef4f
yazan <yazanshrouf149@gmail.com>
2025-05-09 14:41:03 +0300

    docs(linked-list): add example usage demonstrating Linked List

---
+++ exampels/data_structures/lists/linked_list/singly_linked_list_example.dart
@@ -0,0 +1,7 @@
+import 'package:dsa_dart_implementations/data_structures/lists/linked_list/singly_linked_list.dart';
+
+void main() {
+  final SinglyLinkedList<int> singlyLinkedList = SinglyLinkedList<int>();
+  singlyLinkedList.add(1);
+  singlyLinkedList.insert(0, 5);
+}
