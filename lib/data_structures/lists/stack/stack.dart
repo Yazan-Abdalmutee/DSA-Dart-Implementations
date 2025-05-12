87ce9e1ae017f881e7d9ba71c343796944de7175
yazan <yazanshrouf149@gmail.com>
2025-05-12 21:41:45 +0300

    feat(stack): implement stack with core operations

---
+++ lib/data_structures/lists/stack/stack.dart
@@ -0,0 +1,35 @@
+class Stack<T> {
+  final List<T> _stack = [];
+
+  void push(T data) {
+    _stack.add(data);
+  }
+
+  T? pop() {
+    if (!isEmpty()) {
+      return _stack.removeLast();
+    }
+    return null;
+  }
+
+  T? peek() {
+    if (!isEmpty()) {
+      return _stack.last;
+    }
+    return null;
+  }
+
+  int size() {
+    return _stack.length;
+  }
+
+  void clear() {
+    while (_stack.isNotEmpty) {
+      pop();
+    }
+  }
+
+  bool isEmpty() {
+    return _stack.isEmpty;
+  }
+}
