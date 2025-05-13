0b6cc4ace5252d5efa4fea24ed0f2e492348930c
yazan <yazanshrouf149@gmail.com>
2025-05-13 18:59:41 +0300

    docs(linked-queue): add example usage demonstrating Queue using  Linked List

---
+++ exampels/data_structures/lists/queue/linked_queue_example.dart
@@ -0,0 +1,29 @@
+import 'package:dsa_dart_implementations/data_structures/lists/queue/linked_queue.dart';
+
+void main() {
+  final LinkedQueue<String> queue = LinkedQueue<String>();
+
+  queue.enqueue("Yazan");
+  print('--Enqueue--');
+
+  queue.enqueue("Shrouf");
+  print('--Enqueue--');
+
+  queue.enqueue("Ahmad");
+  print('--Enqueue--');
+
+  print('First Element is: ${queue.front()}');
+  print('Size is: ${queue.size()}');
+
+  print('--Dequeue--');
+  queue.dequeue();
+
+  print('First Element is: ${queue.front()}');
+  print('Size is: ${queue.size()}');
+
+  queue.clear();
+  print('--Clear--');
+  print('Size is: ${queue.size()}');
+  print('Is Queue Empty?: ${queue.isEmpty()}');
+  print('First Element is: ${queue.front()}');
+}
